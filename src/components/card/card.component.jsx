ed36ce3821fb1cbb4b61467caf55ac1cdb347778
yoshiki-tanaka999 <shootingstarorufo@gmail.com>
2020-12-08 06:20:09 +0900

    全体レイアウト/フォントを整えた。

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -7,6 +7,7 @@ export const Card = props => (
         {/* <iframe title="vimeo-player" src="{ props.product.url }" width="100%" height="280" frameBorder="0" allowFullScreen></iframe> */}
         <iframe title="vimeo-player" src={ props.product.url } width="100%" height="300" frameBorder="0" allowFullScreen></iframe>
         <h2>{ props.product.name }</h2>
+        <h3>{ props.product.language }</h3>        
         <p>{ props.product.description }</p>
     </div>
 );
---
+++ src/components/contact/contact.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './contact.style.css';
+
+
+export const Contact = () => (
+    <div className='contact'>
+        <div className='contact-content'>
+            <h1> Contact </h1>
+            <p> ご気軽にご連絡下さい。</p>                   
+        </div>
+    </div>
+);
