815bedb3537a5c5a81d1d50759a42d2ed1a3004c
yoshiki-tanaka999 <shootingstarorufo@gmail.com>
2020-12-08 02:33:10 +0900

    headerの修正

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,10 +5,13 @@ import './header.style.css';
 
 export const Header = () => (
     <div className='header'>
-        <AnchorLink href='#products' offset="100">Products</AnchorLink>
-        <AnchorLink href='#biography' offset="100">Biography</AnchorLink>
-        <AnchorLink href='#skills' offset="100">Skills</AnchorLink>
-        <AnchorLink href='#contact' offset="100">Contact</AnchorLink>
-        <AnchorLink href='#aboutme' offset="100">About me</AnchorLink>
+        <div className='header-content'>
+            <AnchorLink href='#products' offset="100">Products</AnchorLink>
+            <AnchorLink href='#biography' offset="100">Biography</AnchorLink>
+            <AnchorLink href='#skills' offset="100">Skills</AnchorLink>
+            <AnchorLink href='#contact' offset="100">Contact</AnchorLink>
+            <AnchorLink href='#aboutme' offset="100">About me</AnchorLink>
+        </div>
+
     </div>
 );
