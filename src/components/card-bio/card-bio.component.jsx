d58ceed9a3e300f14c8cb33f9784636654e3fa51
yoshiki-tanaka999 <shootingstarorufo@gmail.com>
2020-12-08 15:35:07 +0900

    一旦画像データを表示成功

--- src/components/card-bio/card-bio.component.jsx
+++ src/components/card-bio/card-bio.component.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import Image01 from '../card-bio/thumbnail01.jpeg'
 import './card-bio.style.css';
 
 export const CardBio = props => (
     <div className='card-bio-container'>
         {/* <iframe title="vimeo-player" src="{ props.product.url }" width="100%" height="280" frameBorder="0" allowFullScreen></iframe> */}
-        <iframe title="vimeo-player" src={ props.biography.url } width="100%" height="300" frameBorder="0" allowFullScreen></iframe>
+
+        <a href={ props.biography.url } width="100%" height="300" >
+            {/* <img src={ props.biography.img } alt="thumbnail" width="100%" height="100%" /> */}
+            <img src={ Image01} alt="thumbnail" width="100%" height="100%" />
+        </a>
+
         <h2>{ props.biography.name }</h2>      
         <p>{ props.biography.description }</p>
     </div>
