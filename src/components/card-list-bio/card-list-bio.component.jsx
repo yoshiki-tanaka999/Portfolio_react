5cc24eb6efa75d04f027c23f3bf858f78ded2f49
yoshiki-tanaka999 <shootingstarorufo@gmail.com>
2020-12-08 19:16:37 +0900

    画像の表示成功

--- src/components/card-list-bio/card-list-bio.component.jsx
+++ src/components/card-list-bio/card-list-bio.component.jsx
@@ -1,12 +1,33 @@
+// 結果使わない。引数と取得された値がよく分かっていない。
+// 加えて、三項演算子の使い方もこれであっているか、調べる。
+
 import React from 'react';
-import { CardBio } from '../card-bio/card-bio.component';
+import { CardBio01 } from '../card-bio/card-bio01.component';
+import { CardBio02 } from '../card-bio/card-bio02.component';
+import { CardBio03 } from '../card-bio/card-bio03.component';
 import './card-list-bio.style.css';
 
 
+// export const CardListBio = props => (
+//     <div className='card-list-bio'>
+//         {props.biographies.map(biography => (
+//             console.log(biographies)
+//             ({ biography.id } === 1) ? (
+//                 <CardBio01 key= {biography.id} biography={ biography }/>
+//             ):({biography.id} === 2) ? (
+//                 <CardBio02 key= {biography.id} biography={ biography }/>                
+//             ):({biography.id} === 3) ? (
+//                 <CardBio03 key= {biography.id} biography={ biography }/>  
+//             ): console.log('エラー')
+//         ))}
+//     </div>
+// );
+
 export const CardListBio = props => (
     <div className='card-list-bio'>
-        {props.biographies.map(biography => (
-            <CardBio key= {biography.id} biography={ biography }/>
-        ))}
+        {/* {console.log(props.biographies[0])} */}
+        <CardBio01 key= {props.biographies[0].id} biography={ props.biographies[0] }/>
+        <CardBio02 key= {props.biographies[1].id} biography={ props.biographies[1] }/>               
+        <CardBio03 key= {props.biographies[2].id} biography={ props.biographies[2] }/>  
     </div>
 );
